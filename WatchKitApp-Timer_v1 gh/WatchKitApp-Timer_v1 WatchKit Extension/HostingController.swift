7f1438903a7a900bf31032236e1f3563e46310bc
Susan Busse Kersting <31662073+sbusse@users.noreply.github.com>
2020-09-28 14:42:09 -0500

    coinFlipCommit
    commit

---
+++ WatchKitApp-Timer_v1 gh/WatchKitApp-Timer_v1 WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  WatchKitApp-Timer_v1 WatchKit Extension
+//
+//  Created by SUSAN BUSSE KERSTING on 9/28/20.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ WatchKitApp-Timer_v1 gh/WatchKitApp-Timer_v1 WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  WatchKitApp-Timer_v1 WatchKit Extension
+//
+//  Created by SUSAN BUSSE KERSTING on 9/28/20.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
